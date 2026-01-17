586b234f4f49e4581e41e4493153204beb38ffad
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-01-17 13:20:54 -0300

    initial header && its style

---
+++ src/components/sections/Header.tsx
@@ -0,0 +1,15 @@
+import Nav from "../layout/Nav.tsx";
+
+function Header() {
+  return (
+    <>
+      <section className="section section--header">
+        <header>
+          <Nav />
+        </header>
+      </section>
+    </>
+  );
+}
+
+export default Header;
