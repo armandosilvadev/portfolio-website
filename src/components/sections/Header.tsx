2d85eaeefca82aa5782bcae7e71dc13c388cd2a9
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-01-19 13:53:54 -0300

    add new cotainerBtn

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,12 +1,21 @@
 import styles from "./Header.module.css";
 import Nav from "../layout/Nav.tsx";
+import Button from "../ui/Button/Button.tsx";
 
 function Header() {
   return (
     <>
       <section className={`section ${styles.sectionHeader}`}>
-        <header>
+        <header className={`${styles.header} flex flex-center`}>
           <Nav />
+
+          <div className={`${styles.containerBtn} flex`}>
+            <Button id="contact" className="btn btn--contact" text="contact" />
+            <Button
+              className="btn btn--color-mode"
+              text={<i className="fa-regular fa-moon"></i>}
+            />
+          </div>
         </header>
       </section>
     </>
