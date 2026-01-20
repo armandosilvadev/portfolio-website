f49b56071f3dda43c7f5cec07e4604a7369ad45a
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-01-20 18:58:16 -0300

    add new Portfolio section && its initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/sections/Header";
 import Hero from "./components/sections/Hero";
+import Portfolio from "./components/sections/Portfolio";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Portfolio />
     </>
   );
 }
---
+++ src/components/sections/Portfolio.tsx
@@ -0,0 +1,13 @@
+import styles from "./Portfolio.module.css";
+
+function Portfolio() {
+  return (
+    <>
+      <section className={`section ${styles.sectionPortfolio}`}>
+        <h2>Know My Projects</h2>
+      </section>
+    </>
+  );
+}
+
+export default Portfolio;
