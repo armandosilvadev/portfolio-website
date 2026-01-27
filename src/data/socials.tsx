015eded4932334c2367d80b061a306e628318db3
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-01-27 11:23:29 -0300

    create socials files

---
+++ src/data/socials.tsx
@@ -0,0 +1,19 @@
+import type { Social } from "../types/social";
+
+export const socials: Social[] = [
+  {
+    link: "https://github.com/armandosilvadev",
+    icon: <i className="fa-brands fa-github"></i>,
+    text: "Github",
+  },
+  {
+    link: "https://www.linkedin.com/in/armando-silvadev/",
+    icon: <i className="fa-brands fa-square-linkedin"></i>,
+    text: "Linkedin",
+  },
+  {
+    link: "https://wa.me/5585996499718",
+    icon: <i className="fa-brands fa-whatsapp"></i>,
+    text: "WhatsApp",
+  },
+];
