46b84c3484f3f8fdc6a6a7ec94d345ad671e2be0
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-01-29 01:26:35 -0300

    add variables to make sizes responsive

--- src/components/sections/Portfolio.tsx
+++ src/components/sections/Portfolio.tsx
@@ -1,32 +1,12 @@
 import styles from "./Portfolio.module.css";
-import Project from "../ui/Project/Project";
-import logo from "../../assets/img/logo.png";
+// import Project from "../ui/Project/Project";
 
 function Portfolio() {
   return (
     <>
       <section id="portfolio" className={`section ${styles.sectionPortfolio}`}>
         <h2>Come to Know My Projects</h2>
-        <div className={`${styles.containerProjects}`}>
-          <Project
-            className={`${styles.projectItem}`}
-            projectImg={logo}
-            title="Example"
-            description="ASdava sa sjlakvna  aslkjdasd awljie "
-          />
-          <Project
-            className={`${styles.projectItem}`}
-            projectImg={logo}
-            title="Example"
-            description="ASdava sa sjlakvna  aslkjdasd awljie "
-          />
-          <Project
-            className={`${styles.projectItem}`}
-            projectImg={logo}
-            title="Example"
-            description="ASdava sa sjlakvna  aslkjdasd awljie "
-          />
-        </div>
+        <div className={`${styles.containerProjects}`}></div>
       </section>
     </>
   );
