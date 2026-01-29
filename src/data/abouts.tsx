502abcff54eb48687a99cbbfdda4fe8a2e537c21
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-01-29 05:47:28 -0300

    fix wrong text in abouts

--- src/data/abouts.tsx
+++ src/data/abouts.tsx
@@ -6,8 +6,8 @@ export const abouts: About[] = [
     title: <i className="fa-brands fa-react"></i>,
     descriptionText: (
       <>
-        Using <strong>TypeScript</strong> to implement safety in the
-        application, making it scalable and trustable.
+        Using the <strong>React</strong> library to implement component creating
+        dynamic and interactive applications.
       </>
     ),
   },
@@ -16,8 +16,8 @@ export const abouts: About[] = [
     title: <i className="fa-solid fa-code"></i>,
     descriptionText: (
       <>
-        With the use of <strong>CSS</strong> the project will look clean,
-        responsive and really good looking.
+        Using <strong>TypeScript</strong> to implement safety in the
+        application, making it scalable and trustable.
       </>
     ),
   },
