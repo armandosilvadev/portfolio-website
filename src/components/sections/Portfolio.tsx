fe10c6aff080f31fa30a76475a8571e05609fbab
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-02-07 16:35:27 -0300

    remove portfolio's heading

--- src/components/sections/Portfolio.tsx
+++ src/components/sections/Portfolio.tsx
@@ -5,7 +5,6 @@ function Portfolio() {
   return (
     <>
       <section id="portfolio" className={`section ${styles.sectionPortfolio}`}>
-        <h2>Come to Know My Projects</h2>
         <div className={`${styles.containerProjects}`}></div>
       </section>
     </>
