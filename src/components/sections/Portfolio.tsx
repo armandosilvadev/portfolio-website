0bd60a7d2011ba7e16b824a7a52d869fa12e3192
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-07-25 12:45:17 -0300

    add media queries

--- src/components/sections/Portfolio.tsx
+++ src/components/sections/Portfolio.tsx
@@ -1,13 +1,30 @@
-import { projects } from "../../data/projects";
-import Projects from "../ui/Projects/Projects";
-import styles from "./Portfolio.module.css";
+import { projects } from '../../data/projects';
+import Button from '../ui/Button/Button';
+import Projects from '../ui/Projects/Projects';
+import styles from './Portfolio.module.css';
 
 function Portfolio() {
   return (
     <>
-      <section id="portfolio" className={`section ${styles.sectionPortfolio}`}>
+      <section
+        id='portfolio'
+        className={`section ${styles.sectionPortfolio}`}
+      >
         <div className={`${styles.containerProjects}`}>
-          <Projects projects={projects}/>
+          <Projects projects={projects} />
+        </div>
+        <div className={styles.showMoreBtnContainer}>
+          <Button
+            text={
+              <>
+                <span>see more</span>
+                <br></br>
+                <i className='fa-solid fa-angle-down'></i>
+              </>
+            }
+            id='btn--show-more'
+            className='btn btn--show-more'
+          />
         </div>
       </section>
     </>
