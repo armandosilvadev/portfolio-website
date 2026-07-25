3c21870389c961d18da6a57a3c7c15170f29169e
argson <228495808+armandosilvadev@users.noreply.github.com>
2026-07-25 12:56:12 -0300

    add show more projects feature

--- src/components/sections/Portfolio.tsx
+++ src/components/sections/Portfolio.tsx
@@ -1,25 +1,45 @@
+import { useState } from 'react';
 import { projects } from '../../data/projects';
 import Button from '../ui/Button/Button';
 import Projects from '../ui/Projects/Projects';
 import styles from './Portfolio.module.css';
 
 function Portfolio() {
+  const [showMore, setShowMore] = useState<string>('');
+  const [btnText, setBtnText] = useState<string>('show more');
+  const [btnIcon, setBtnIcon] = useState<string | React.ReactNode>(
+    <i className='fa-solid fa-angle-down'></i>,
+  );
+
+  const handleShowMore = () => {
+    if (!showMore) {
+      setShowMore('show');
+      setBtnText('show less');
+      setBtnIcon(<i className='fa-solid fa-angle-up'></i>);
+    } else {
+      setShowMore('');
+      setBtnText('show more');
+      setBtnIcon(<i className='fa-solid fa-angle-down'></i>);
+    }
+  };
+
   return (
     <>
       <section
         id='portfolio'
         className={`section ${styles.sectionPortfolio}`}
       >
-        <div className={`${styles.containerProjects}`}>
+        <div className={`${styles.containerProjects} ${styles[showMore]}`}>
           <Projects projects={projects} />
         </div>
         <div className={styles.showMoreBtnContainer}>
           <Button
+            onClick={handleShowMore}
             text={
               <>
-                <span>see more</span>
+                <span>{btnText}</span>
                 <br></br>
-                <i className='fa-solid fa-angle-down'></i>
+                {btnIcon}
               </>
             }
             id='btn--show-more'
